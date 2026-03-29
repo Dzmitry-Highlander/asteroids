fb16b9c222eaf789253d9dfea47fcdaef61f1004
Dzmitry-Highlander <strixmymail@gmail.com>
2026-03-29 09:58:01 +0300

    update main

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ def main():
                 return
         
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick() / 1000
+        dt = (clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
