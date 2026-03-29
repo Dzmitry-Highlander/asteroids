2218cbf0fe474ad9b3838dc61ed9912efa279750
Dzmitry-Highlander <strixmymail@gmail.com>
2026-03-29 08:36:27 +0300

    update main, fps = 60

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
 
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
         
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000
+        print(dt)
+
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
